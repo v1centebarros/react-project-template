187929e56d9ca2f4fcc77bb90fe6d652f7664f16
v1centebarros <vmabarros@ua.pt>
2024-02-05 22:50:00 +0000

    Add router and home router

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,5 @@
+interface HomeProps {}
+
+export const Home: React.FunctionComponent<HomeProps> = (props) => {
+  return <div className={"text-2xl text-orange-500 font-bold"}>Hello World</div>;
+};
---
+++ src/utils/router.tsx
@@ -0,0 +1,11 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Home} from "@/pages/home";
+
+export default function Router() {
+
+    const router = createBrowserRouter([{
+        path: "/",
+        element: <Home />
+    }])
+    return <RouterProvider router={router} />
+}
