544a26078616e785d6bf9f9555b38db9d64dca31
v1centebarros <vmabarros@ua.pt>
2024-02-05 22:51:01 +0000

    Remove props

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,5 @@
 interface HomeProps {}
 
-export const Home: React.FunctionComponent<HomeProps> = (props) => {
+export const Home: React.FunctionComponent<HomeProps> = () => {
   return <div className={"text-2xl text-orange-500 font-bold"}>Hello World</div>;
 };
